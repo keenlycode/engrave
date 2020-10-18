a3728d33a908860181ceb32306633afb6687a577
Nitipit Nontasuwan <nitipit@gmail.com>
2020-10-18 15:31:00 +0700

    init

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pillari",
+    version="0.0.1",
+    author="Nitipit Nontasuwan",
+    author_email="nitipit@gmail.com",
+    description="Static website generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'pillari=pillari:run'
+        ]
+    },
+    install_requires=[
+        'jinja2==2.11.2',
+        'mistune==2.0.0a5',
+        'watchgod==0.6',
+    ]
+)
