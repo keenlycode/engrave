a5a3c5ec3368159cd8939fe5a2020c275fec4cf1
Nitipit Nontasuwan <nitipit@gmail.com>
2021-04-11 02:12:21 +0700

    update

--- docs-src/base.js
+++ docs-src/base.js
@@ -1,9 +1,6 @@
 import 'packet-ui/dist/packet-ui.js';
-import './_base.scss';
 import hljs from 'highlight.js/lib/core';
 import bash from 'highlight.js/lib/languages/bash';
-import 'highlight.js/styles/github.css';
 
 hljs.registerLanguage('bash', bash);
-
 hljs.highlightAll();
