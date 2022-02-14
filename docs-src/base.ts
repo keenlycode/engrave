8b7e816c29572dd6e54ba850943eed367f361c8e
Nitipit Nontasuwan <nitipit@gmail.com>
2022-02-15 00:49:40 +0700

    update

--- docs-src/base.ts
+++ docs-src/base.ts
@@ -1,17 +1,24 @@
-import hljs from 'highlight.js/lib/core';
-import bash from 'highlight.js/lib/languages/bash';
 import { define } from 'gadjet/src/ui/ui';
 import { Badge } from 'gadjet/src/ui/badge/badge';
+import { html, render } from 'uhtml';
 
-hljs.registerLanguage('bash', bash);
-hljs.highlightAll();
+import Prism from 'prismjs';
+import 'prismjs/components/prism-bash';
 
 define('el-badge', Badge);
 
 class Code extends HTMLElement {
     _lang: string;
+    _innerHTML: string;
     constructor() {
         super();
         this._lang = this.getAttribute("lang");
+        this._innerHTML = this.innerHTML;
+        this.innerHTML = Prism.highlight(
+            this._innerHTML,
+            Prism.languages.bash,
+            'javascript'
+        )
     }
 }
+customElements.define('el-code', Code);
