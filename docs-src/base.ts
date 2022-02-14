a34c6ba36f51670f939773083d034a5e9007056e
Nitipit Nontasuwan <nitipit@gmail.com>
2022-02-15 00:11:29 +0700

    update

--- docs-src/base.ts
+++ docs-src/base.ts
@@ -1,8 +1,10 @@
-import {define} from 'gadjet/src/ui/ui';
-import {Badge} from 'gadjet/src/ui/badge/badge';
-import hljs from 'highlight.js';
+import hljs from 'highlight.js/lib/core';
 import bash from 'highlight.js/lib/languages/bash';
+import { define } from 'gadjet/src/ui/ui';
+import { Badge } from 'gadjet/src/ui/badge/badge';
+
 hljs.registerLanguage('bash', bash);
+hljs.highlightAll();
 
 define('el-badge', Badge);
 
