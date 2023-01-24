736de312b7f52f4e8d1c03d0912d0064b345c39c
Nitipit Nontasuwan <nitipit@gmail.com>
2023-01-24 10:14:44 +0700

    update

--- docs.py
+++ docs.py
@@ -1,11 +1,23 @@
 import asyncio
 from pathlib import Path
+import shutil
 from engrave import Engrave
 
 
 _dir = Path(__file__).parent
 
-async def parcel():
+
+async def asset():
+    print('Copy assets...')
+    shutil.copytree(
+        'docs-src/asset/',
+        'docs/asset/',
+        dirs_exist_ok=True
+    )
+    print('Copy assets... Finished')
+
+
+async def docs():
     cmd = f"npx parcel watch 'docs-src/**/*.(scss|js|ts|png|jpg|svg)' --dist-dir='docs'"
     proc = await asyncio.create_subprocess_shell(cmd)
     await proc.communicate()
@@ -14,11 +26,12 @@ async def main():
     engrave = Engrave(
         src_dir=_dir.joinpath('docs-src'),
         dest_dir=_dir.joinpath('docs'))
-    addr = '127.0.0.1'
+    addr = '0.0.0.0'
     port = 8000
 
+    await asset()
     await asyncio.gather(
-        parcel(),
+        docs(),
         engrave.dev(),
         engrave.run_server(addr, port),
     )
